6d20aa82d22f730a8cc6b4cb67fa0b723f34742b
jpcamba <camba.jannieca@gmail.com>
2019-07-09 08:07:25 +0800

    Set up configurations for Firebase database

---
+++ scripts/model.js
@@ -0,0 +1,13 @@
+// Initialize Firebase database
+const firebaseConfig = {
+    apiKey: "AIzaSyBY5RRgbzzEAxu3H8PHhLRjMLLC-Fr6RVk",
+    authDomain: "npv-calculator-jpc.firebaseapp.com",
+    databaseURL: "https://npv-calculator-jpc.firebaseio.com",
+    projectId: "npv-calculator-jpc",
+    storageBucket: "",
+    messagingSenderId: "897313862825",
+    appId: "1:897313862825:web:5ec5d63427172545"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database().ref().child('NPV');
